f2b4f15832979b1e304bcceec636b8b8fba59bf6
Alberto Damo <alberto.damo@gmail.com>
2024-12-06 15:34:28 +0100

    created this branch for the awful editors that are not able to include a librarie without exploding for the tests inside of it. I'm talking of Aurix Development Studio of course

--- test/.dummy/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include "./flash_memory/flash_memory.h"
-#include "./test_flash/hw_flash.h"
-#include "./test_score_lib/test_lib.h"
-
-int main(int argc __attribute_maybe_unused__, char *argv[] __attribute_maybe_unused__)
-{
-    return EXIT_SUCCESS;
-}
