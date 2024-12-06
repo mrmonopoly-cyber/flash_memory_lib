c6b22b8da043dc5975a135eb6273845c463d71a3
Alberto Damo <alberto.damo@gmail.com>
2024-12-06 19:58:54 +0100

    added a few tests for writing

--- test/tests/write/main.c
+++ test/tests/write/main.c
@@ -36,7 +36,7 @@ static int store_new_var(PagePool_t* pool,FlashDecriptor_t* o_var_id, const Stor
 
 int main(int argc __attribute_maybe_unused__, char *argv[] __attribute_maybe_unused__)
 {
-    const uint8_t pool_size = 2;
+    const uint8_t pool_size = 20;
     STACK_PAGEPOOL_T(pool, pool_size, );
 
     if (init_pool(pool, pool_size) < 0) {
@@ -56,6 +56,87 @@ int main(int argc __attribute_maybe_unused__, char *argv[] __attribute_maybe_unu
         return -2;
     }
 
+    char bb = 'b';
+    FlashDecriptor_t fd_b;
+    const StoreNewValueInputArgs_t bb_args = {
+        .value = &bb,
+        .data_type = UINT8,
+        .var_description = "a variable containing a character \'a\'"
+    };
+    if (store_new_var(pool,&fd_b, &bb_args) < 0) {
+        FAILED("failed storing a var");
+        return -2;
+    }
+
+
+    char aa_new = 126;
+    const UpdateValueInputArgs_t update_aa = {
+        .var_id = fd_a,
+        .new_value = &aa_new,
+        .size_new_value = sizeof(aa_new),
+    };
+    int err = flash_memory_update_value(pool, &update_aa);
+    if (err < 0) {
+        FAILED("failed updating var a with error:");
+        fprintf(stderr, "%d\n",err);
+    }else{
+        PASSED("var a updated");
+    }
+
+    char bb_new = 127;
+    const UpdateValueInputArgs_t update_bb = {
+        .var_id = fd_b,
+        .new_value = &bb_new,
+        .size_new_value = sizeof(bb_new),
+    };
+    err = flash_memory_update_value(pool, &update_bb);
+    if (err < 0) {
+        FAILED("failed updating var b with error:");
+        fprintf(stderr, "%d\n",err);
+    }else{
+        PASSED("var b updated");
+    }
+
+    const UpdateValueInputArgs_t update_invalid_id = {
+        .var_id = fd_b + 12,
+        .new_value = &bb_new,
+        .size_new_value = sizeof(bb_new),
+    };
+    err = flash_memory_update_value(pool, &update_invalid_id);
+    if (err < 0) {
+        PASSED("blocked updating invalid id var with error:");
+        fprintf(stderr, "%d\n",err);
+    }else{
+        FAILED("invalid id var updated");
+    }
+
+    const UpdateValueInputArgs_t update_invalid_null_new_value = {
+        .var_id = fd_b + 12,
+        .new_value = NULL,
+        .size_new_value = sizeof(bb_new),
+    };
+    err = flash_memory_update_value(pool, &update_invalid_null_new_value);
+    if (err < 0) {
+        PASSED("blocked updating invalid new_value null var with error:");
+        fprintf(stderr, "%d\n",err);
+    }else{
+        FAILED("invalid var updated with new value NULL");
+    }
+
+    const UpdateValueInputArgs_t update_invalid_zero_size_new_value = {
+        .var_id = fd_b + 12,
+        .new_value = &bb_new,
+        .size_new_value = 0,
+    };
+    err = flash_memory_update_value(pool, &update_invalid_zero_size_new_value);
+    if (err < 0) {
+        PASSED("blocked updating invalid zero size new_value var with error:");
+        fprintf(stderr, "%d\n",err);
+    }else{
+        FAILED("invalid var updated with zero size new_value");
+    }
+
+    print_status_flash();
 
     print_SCORE();
 
